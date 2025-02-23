672ef99ae116ade79b575cbe46057cd2064566c2
theonlyharsh <creator.zuni@gmail.com>
2025-02-23 16:52:24 +0000

    main file

---
+++ app.py
@@ -0,0 +1,10 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def introduce(self):
+        print(f"My name is {self.name} and I am {self.age} years old.")
+
+p = Person("Harshad", 30)
+p.introduce()
